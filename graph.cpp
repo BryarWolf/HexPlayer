f3fc8b944aae1c0456d0ffd451f991245cb2a913
Michael Wright <BryarWolf@yahoo.com>
2013-11-01 07:53:11 -0400

    Graph initialization working

--- graph.cpp
+++ graph.cpp
@@ -29,37 +29,57 @@ public:
   void addEdge(Vertex nextV, float cost){
     edges.push_back(Edge(&nextV, cost));
   }
+  int getIndex(){
+    return index;
+  }
 };
 
 class Graph{
   std::list<Vertex> vertices;
 public:
+  // function to initialize a graph with the specified number of vertices (numV),
+  // graph density (density) and edge cost (range)
+  // NOTE: the density value passed in and used was multiplied by 100 for use in
+  // the edge creation tests
   void initGraph(int numV, float density, int range){
-    float cost;
+    int edgeCount = 0;
+    float cost, avgEdgeCost = 0.0;
+    std::list<Vertex>:: iterator j,k;
+
     // create the vertex list
     for (int i=0; i<numV; i++){
       vertices.push_back(Vertex(i));
     }
+    std::cout << "This graph has " << numV << " vertices." << std::endl;
 
     // initialize random number generator
     srand(time(NULL));
 
-    // iterate through the list of vertices to create edges 
-    for (std::list<Vertex>::iterator j=vertices.begin(); j != vertices.end(); j++){
-      // iterate through the list again to test for edges betwenn j and k
-      for (std::list<Vertex>::iterator k=vertices.begin(); k != vertices.end(); k++) {
-	if (j == k) break;  // Ignore circular edges
+    // iterate through the list of vertices to create edges
+    // might be better to use while, but this works
+    for (j=vertices.begin(); j != vertices.end(); j++){
+      // iterate through the list again to test for edges between j and k
+      for (k=j; k != vertices.end(); k++) {
+	if (j == k) continue;  // Ignore circular edges
 	else {
 	  // test a random number to see if it is less than or equal to density
 	  if ((rand() % 100) <= density) {
 	    // if test passed, add edge between vertex i and j with a random cost
 	    // between 1 and 10
 	    cost = rand() % range + 1;
+	    // std::cout << "New edge from " << j->getIndex() << " to ";
+	    // std::cout << k->getIndex() << " with a cost of " << cost << std::endl;
+	    avgEdgeCost += cost;
 	    j->addEdge(*k, cost);
+	    edgeCount++;
 	  }
 	}
       }
     }
+    avgEdgeCost /= edgeCount;
+    std::cout << "With Graph Density=" << density << "%, " << edgeCount;
+    std::cout << " edges were created." << std::endl;
+    std::cout << "The average edge cost is " << avgEdgeCost << "." << std::endl;
   }
   int numVertices() {
     return vertices.size();
@@ -74,14 +94,12 @@ int main(int argc, char *argv[]) {
   int numVertices = atoi(argv[1]); // Holds user entry for number of nodes
   float graphDensity = atof(argv[2]); // Holds user entry for graph density
   int maxRange = atoi(argv[3]); // Holds user entry for maximum range of edges
-  std::cout << "User Entry for graph size: " << numVertices << std::endl;
-  std::cout << "Graph density: " << graphDensity << std::endl;
-  std::cout << "Distance range: 1-" << maxRange << std::endl;
-  std::cout << "Average path length: TBD" << std::endl;
+  std::cout << "User Entries: Graph size=" << numVertices;
+  std::cout << ", Graph density=" << graphDensity;
+  std::cout << ", Range=1-" << maxRange << std::endl << std::endl;
 
   // Define and initialize a graph using the user inputs
   Graph tGraph;
   tGraph.initGraph(numVertices, graphDensity*100, maxRange);
-  std::cout << "Graph has " << tGraph.numVertices() << " vertices" << std::endl;
 }
 
